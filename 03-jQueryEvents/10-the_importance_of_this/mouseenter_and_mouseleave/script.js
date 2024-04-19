dd627bd3cad6e06f8d31d2167316335501209757
akramalex <info@quick4you.net>
2024-04-19 15:37:09 +0100

    03

--- 03-jQueryEvents/10-the_importance_of_this/mouseenter_and_mouseleave/script.js
+++ 03-jQueryEvents/10-the_importance_of_this/mouseenter_and_mouseleave/script.js
@@ -5,3 +5,11 @@ $('button').mouseenter(function() {
 $('button').mouseleave(function() {
 	$(this).removeClass('makeBlue').addClass('makeRed');
 });
+
+// $('p').click(function(){
+// 	$(this).text("Changed only the text for this paragraph");
+// });
+
+// $('.card').mouseenter(function(){
+// 	$(this).fadeTo(2000, 0.2).fadeTo(2000, 1);
+// });
